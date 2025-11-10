cd51097e77f51242c909add2ff493bf0995fd54e
Yunus <yunusabri2049@gmail.com>
2025-11-10 20:43:07 +0300

    Finalizing the frontend

--- finance/urls.py
+++ finance/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("dashboard", views.dashboard, name='dashboard'),
+    path("/transaction_list", views.transaction_list, name='transaction_list'),
+    path("/transaction_form", views.transaction_form, name='transaction_form'),
+    path("/category_list", views.category_list, name='category_list'),
+    path("/category_form", views.category_form, name='category_form')
 ]
--- finance/views.py
+++ finance/views.py
@@ -2,5 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def dashboard(request):
+    return render(request, "finance/dashboard.html")
+
+
+def transaction_list(request):
+    return render(request, "finance/transaction_list.html")
+
+
+def transaction_form(request):
+    return render(request, "finance/transaction_form.html")
+
+
+def category_list(request):
+    return render(request, "finance/category_list.html")
+
+
+def category_form(request):
+    return render(request, "finance/category_form.html")
+
+
+def logout_fun(request):
     pass
