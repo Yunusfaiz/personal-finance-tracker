0419533b84d985c820ad2bce726a5d399f66c5fd
Yunus <yunusabri2049@gmail.com>
2025-11-12 22:58:36 +0300

    user registration and authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login, name='login'),
-    path("/profile", views.profile, name='profile'),
-    path("/register", views.register, name='register')
+    path('', views.login_user, name='login_user'),
+    path("login_user", views.login_user, name='login_user'),
+    path("profile", views.profile, name='profile'),
+    path("register", views.register, name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.urls import reverse, include
 # Create your views here.
 
-def login(request):
-    return render(request, "accounts/login.html")
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Logged in successfully!')
+            print("login success")
+            return redirect('profile')
+        else: 
+            messages.error(request, 'Invalid username or password. Try again.')
+            print("login failed")
+            return redirect('login')
+    else:
+        return render(request, "accounts/login.html", {})
 
 
 def profile(request):
