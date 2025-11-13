c69f6839eabe602bc8486dbef9a77660ed06e1ed
Yunus <yunusabri2049@gmail.com>
2025-11-13 20:04:26 +0300

    User authentication and registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_user, name='login_user'),
-    path("login_user", views.login_user, name='login_user'),
+    path('', views.login_user, name='login'),
     path("profile", views.profile, name='profile'),
-    path("register", views.register, name='register')
+    path("register", views.register_user, name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.urls import reverse, include
-# Create your views here.
+from django.contrib.auth.models import User
 
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
+# Create your views here.
 
 def login_user(request):
     if request.method == 'POST':
@@ -16,20 +13,53 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, 'Logged in successfully!')
             print("login success")
-            return redirect('profile')
-        else: 
-            messages.error(request, 'Invalid username or password. Try again.')
+            return render(request, 'registration/profile.html', {
+                'message': 'Logged in successfully'
+            })
+        else:
             print("login failed")
-            return redirect('login')
+            return render(request, 'registration/login.html', {
+                'message': "Wrong credentials try again"
+            })
     else:
-        return render(request, "registration/login.html", {})
+        return render(request, "registration/login.html", {
+            'message': "Error occured please try again"
+        })
 
 
 def profile(request):
     return render(request, "registration/profile.html")
 
 
-def register(request):
-    return render(request, "registration/register.html")
+def register_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password != password2:
+            return render(request, 'registration/register.html', {
+                'error_message': "passwords don't match"
+            })
+        
+        elif User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+            return render(request, 'registration/register.html', {
+                'error_message': "Username or email already exists"
+            })
+        
+        else:
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+            user.save()
+
+            return render(request, 'registration/login.html', {
+                'message': 'User created!'
+            })
+    
+    else:
+        return render(request, "registration/register.html")
